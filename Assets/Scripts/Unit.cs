2ba34c52fde2988ce3f110e4d314ee810f941c90
GDFauxtrot <gregduarte97@gmail.com>
2016-05-01 18:08:58 -0700

    Created Tree Generation GameObject
    Created tree generation gameobject/script. Scene is set up to generate
    1000 trees, 10 per unit per 10x10 area (all randomly scattered).

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+	private GameObject obj;
+	private Vector2 size;
+
+	public Unit (GameObject obj, Vector2 size) {
+		this.obj = obj;
+		this.size = size;
+	}
+
+	public void setObject(GameObject obj) {
+		this.obj = obj;
+	}
+
+	public void setSize(Vector2 size) {
+		this.size = size;
+	}
+
+	public void setSize(float x, float y) {
+		this.size = new Vector2(x,y);
+	}
+
+	public Vector2 getSize() {
+		return size;
+	}
+
+	public GameObject getObj() {
+		return obj;
+	}
+}
---
+++ Assets/TreeHeight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeHeight : MonoBehaviour {
+
+	void Start () {
+		transform.Find("Branches").transform.localPosition = new Vector3(0f, Random.Range(0.7f, 1.5f), 0f);
+		gameObject.isStatic = true;
+		foreach (Transform child in transform) {
+			child.gameObject.isStatic = true;
+			if (child.name == "Branches") {
+				foreach (Transform branchChild in child) {
+					branchChild.gameObject.isStatic = true;
+				}
+			}
+		}
+			
+	}
+	
+	void Update () {
+	
+	}
+}
